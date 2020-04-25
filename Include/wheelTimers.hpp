ca6e4d1680aa24eb07123abf0bb6b0b666e56f04
niktob560 <niktob560@gmail.com>
2020-04-25 12:22:43 +0300

    Created funcs

--- Include/wheelTimers.hpp
+++ Include/wheelTimers.hpp
@@ -6,8 +6,9 @@ namespace wheelTimers
 	extern uint64_t ticks0, 
 					ticks1, 
 					ticks2;
+	extern const uint64_t baseFreq;
 
 	void init();
 	void stop();
-	void setFreqs(uint8_t w0, uint8_t w1, uint8_t w2);
+	void setFreqs(uint64_t w0, uint64_t w1, uint64_t w2);
 } // namespace wheelTimers
--- Src/wheelTimers.cpp
+++ Src/wheelTimers.cpp
@@ -6,7 +6,27 @@ namespace wheelTimers
 				ticks1 = 0, 
 				ticks2 = 0;
 
-	void init();
-	void stop();
-	void setFreqs(uint8_t w0, uint8_t w1, uint8_t w2);
+	extern const uint64_t baseFreq = (160000000L) / 256;
+
+	void init()
+	{
+		TIMER3Init(TIMER3_COMA_TOGGLE | TIMER3_COMB_TOGGLE | TIMER3_COMC_TOGGLE, TIMER3_WF_CTC_TOPICR3, TIMER3_CLK_SRC_256);
+		TIMER3SetA(0);
+		TIMER3SetB(0);
+		TIMER3SetC(0);
+	}
+
+	void stop()
+	{
+		TIMER3SetA(0);
+		TIMER3SetB(0);
+		TIMER3SetC(0);
+	}
+
+	void setFreqs(uint64_t w0, uint64_t w1, uint64_t w2)
+	{
+		TIMER3SetA(w0);
+		TIMER3SetB(w1);
+		TIMER3SetC(w2);
+	}
 } // namespace wheelTimers
