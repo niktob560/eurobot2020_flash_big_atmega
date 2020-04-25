7be57fff00a2358dae405c1c5447220778fca365
niktob560 <niktob560@gmail.com>
2020-04-25 12:38:21 +0300

    Added counters of ticks

--- Src/wheelTimers.cpp
+++ Src/wheelTimers.cpp
@@ -8,12 +8,31 @@ namespace wheelTimers
 
 	extern const uint64_t baseFreq = (160000000L) / 256;
 
+	void _COMA_ISR()
+	{
+		ticks0++;
+	}
+	void _COMB_ISR()
+	{
+		ticks1++;
+	}
+	void _COMC_ISR()
+	{
+		ticks2++;
+	}
+
 	void init()
 	{
 		TIMER3Init(TIMER3_COMA_TOGGLE | TIMER3_COMB_TOGGLE | TIMER3_COMC_TOGGLE, TIMER3_WF_CTC_TOPICR3, TIMER3_CLK_SRC_256);
 		TIMER3SetA(0);
 		TIMER3SetB(0);
 		TIMER3SetC(0);
+		interrupt::set(interrupt::TC3_COMPA, _COMA_ISR);
+		interrupt::set(interrupt::TC3_COMPB, _COMB_ISR);
+		interrupt::set(interrupt::TC3_COMPC, _COMC_ISR);
+		TIMER3EnableCOMPAInterrupt();
+		TIMER3EnableCOMPBInterrupt();
+		TIMER3EnableCOMPCInterrupt();
 	}
 
 	void stop()
